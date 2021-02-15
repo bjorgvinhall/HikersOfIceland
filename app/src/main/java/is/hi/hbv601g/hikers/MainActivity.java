08a8c96f1fc1a3ce695004dbb9983b03719fb288
bjorgvinhall <43784017+bjorgvinhall@users.noreply.github.com>
2021-02-15 21:13:14 +0000

    init

---
+++ app/src/main/java/is/hi/hbv601g/hikers/MainActivity.java
@@ -0,0 +1,14 @@
+package is.hi.hbv601g.hikers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
