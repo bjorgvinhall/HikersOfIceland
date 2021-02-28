033331ebc8aab5631207d281de8774cf1ab45949
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-02-28 22:05:19 +0000

    RESTy stuff

--- app/src/main/java/is/hi/hbv601g/hikers/MainActivity.java
+++ app/src/main/java/is/hi/hbv601g/hikers/MainActivity.java
@@ -2,7 +2,18 @@ package is.hi.hbv601g.hikers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.VoiceInteractor;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String URL="https://api.github.com/emojis";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("onResponse: Rest Response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("onResponse: Error Response", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
+
     }
 }
