49fa638eaa5b99a907e74e2e75db56c70b26b228
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-03-21 23:14:09 +0000

    feat: login

--- app/src/main/java/is/hi/hbv601g/hikers/Networking/Service.java
+++ app/src/main/java/is/hi/hbv601g/hikers/Networking/Service.java
@@ -1,17 +1,24 @@
 package is.hi.hbv601g.hikers.Networking;
 
 import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONObject;
 
 import java.lang.reflect.Type;
 import java.util.List;
 
 import is.hi.hbv601g.hikers.Entities.Hike;
+import is.hi.hbv601g.hikers.Entities.Profile;
+import is.hi.hbv601g.hikers.R;
 
 public class Service {
-    private static final String BASEURL = "http:10.0.2.2:8080/rest/";
+    private static final String TAG = "YOLO";
+    private static final String BASEURL = "https://hikers-of-iceland.herokuapp.com/rest/";
     RequestHelper mRequestHelper;
 
     public Service(Context context) {
@@ -35,7 +42,19 @@ public class Service {
         });
     }
 
+    public void postLogin(JSONObject requestBody, NetworkCallback<Profile> callback){
+        mRequestHelper.post(BASEURL + "login", requestBody, new NetworkCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                Gson gson = new Gson();
+                Profile profile = gson.fromJson(result, Profile.class);
+                callback.onSuccess(profile);
+            }
 
-
-
+            @Override
+            public void onFailure(String error) {
+                callback.onFailure(error);
+            }
+        });
+    }
 }
