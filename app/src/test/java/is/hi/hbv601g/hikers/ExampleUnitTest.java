4af3305406c82e87e1b794a8dc39726d3233de98
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-04-17 17:06:12 +0000

    tests empty and full

--- app/src/test/java/is/hi/hbv601g/hikers/ExampleUnitTest.java
+++ app/src/test/java/is/hi/hbv601g/hikers/ExampleUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-import is.hi.hbv601g.hikers.Networking.Service.*;
+import is.hi.hbv601g.hikers.Entities.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -18,9 +18,10 @@ public class ExampleUnitTest {
     }
 
     @Test
+    // Profile class can create an empty object
     public void subraction_isCorrect() {
-
+        Profile profile = new Profile();
+        boolean actualAnswer = profile.isEmpty();
+        assertTrue(actualAnswer);
     }
-
-
 }
